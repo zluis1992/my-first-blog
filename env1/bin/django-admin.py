77a4f24ab1ec6966d216c5a65f9d218b4fa11137
zluis1992 <zluisigloxx@gmail.com>
2018-04-14 11:37:31 -0500

    Creacion del modelo Post 2da parte

--- env1/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ingluisavilag/Documentos/CursoPython/Semana2/Ejercicios/env1/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
